581e40bd656e354d10d8ce389be94a22dddef1ae
Andrey Antukh <niwi@niwi.nz>
2024-10-22 16:51:29 +0200

    Update cli tool

--- src/svgoCli.js
+++ src/svgoCli.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
+const process = require("process");
 const svgo = require("./svgo.js");
-const input = fs.readFileSync(0, 'utf-8');
-const output = svgo.optimize(input, svgo.defaultOptions);
-process.stdout.write(output);
-process.exit(0);
+
+try {
+  const input = fs.readFileSync(0, {encoding: 'utf-8'});
+  const output = svgo.optimize(input, svgo.defaultOptions);
+  fs.writeFileSync(1, output, {encoding: 'utf-8', flush: true});
+  process.exit(0);
+} catch (cause) {
+  console.error(cause);
+  process.exit(1);
+}
